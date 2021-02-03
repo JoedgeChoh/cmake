afb14a9b50bc3f398a34f4411ab87431a201f23a
zhuhe <zhuhe@weichai.com>
2021-02-02 22:19:04 -0500

    cmake1-2

---
+++ test/include/demoConfig.h
@@ -0,0 +1,4 @@
+// the configured options and settings for demo
+#define demo_VERSION_MAJOR 1
+#define demo_VERSION_MINOR 0
+/* #undef USE_MYMATH */
--- test/src/main.cxx
+++ test/src/main.cxx
@@ -1,18 +1,30 @@
 // A simple program that computes the square root of a number
 #include <stdio.h>
 #include <stdlib.h>
+#include "demoConfig.h"
+#ifdef USE_MYMATH
 #include "mymath.h"
+#endif
 #include<math.h>
 int main (int argc, char *argv[])
 {
   if (argc < 2)
     {
-    // fprintf(stdout,"%s version: %d.%d" ,argv[0],);
+    fprintf(stdout,"%s version: %d.%d \n", argv[0],demo_VERSION_MAJOR,demo_VERSION_MINOR);
     fprintf(stdout,"Usage: %s number\n",argv[0]);
     return 1;
     }
   double inputValue = atof(argv[1]);
-  double outputValue = sqrt(inputValue);
+
+# ifdef USE_MYMATH 
+  fprintf(stdout,"USE_MYMATH \n");
+  double outputValue = mysqrt(inputValue);
+  
+# else
+   fprintf(stdout,"USE_MATH \n");
+   double outputValue = sqrt(inputValue);
+# endif
+
   fprintf(stdout,"The square root of %g is %g\n",inputValue, outputValue);
   return 0;
 }
